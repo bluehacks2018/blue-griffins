bda2412072b4a3c28f4f2c735648accf5997fe73
hallardajm <joana.mae321@gmail.com>
2018-01-28 01:51:00 +0800

    log-in and registration

--- log-in.php
+++ log-in.php
@@ -1,18 +1,31 @@
- <!DOCTYPE html>
+ 
   <html>
     <head>
       <!--Import Google Icon Font-->
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <!--Import materialize.css-->
       <link type="text/css" rel="stylesheet" href="materialize/css/materialize.min.css"  media="screen,projection"/>
-
+      <script src="materialize/js/jquery-3.3.1.min.js"></script>
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
      <title>Log in</title>
+
+     <script>
+   $(document).ready(function() {
+    Materialize.updateTextFields();
+  });
+</script>
+    <!--Import jQuery before materialize.js-->
+
+   <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+      <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+       <script type="text/javascript" src="materialize/js/materialize.js"></script>
     </head>
 
     <body>
+     
 
+ 
 		<nav>
     <div class="nav-wrapper blue-grey darken-1">
       <a href="Main-Page.php" class="brand-logo">Logo</a>
@@ -26,10 +39,11 @@
       </ul>
     </div>
   </nav>
-        
+        <form name="form1" action="" method="post">
         <br><br><br> 
         <center></center>
     <div class="container">
+
        <div class="row valign-wrapper">
         <div class="col s6 offset-s3 valign">
           <div class="card blue-grey darken-1">
@@ -39,15 +53,15 @@
                   <div class="row white-text">
                     <br>
     <div class="input-field col s12">
-      <input id="email" type="text" class="validate">
-      <label class="active white-text" for="email" style="font-size: 20px;">Email</label>
+      <input name="email" type="text" class="validate">
+      <label class="active white-text" for="email" style="font-size: 20px;" >Email</label>
     </div>
     <div class="input-field col s12">
-      <input id="pword" type="text" class="validate">
-      <label class="active white-text" for="pword" style="font-size: 20px;">Password</label>
+      <input name="pword" type="text" class="validate">
+      <label class="active white-text" for="pword" style="font-size: 20px;" >Password</label>
     </div>
     <div class="input-field col s6 offset-s4">
-      <a class="waves-effect waves-light btn blue-grey lighten-5 blue-grey-text text-darken-1" id="signin" style="font-weight: bolder;">Sign in</a>
+      <input class="btn btn-default submit " type="submit" name="log" value="Log-in">
     </div>
   </div>
             </div>
@@ -55,22 +69,42 @@
         </div>
       </div>
     </div>
-       
+      </form> 
 
           
       
+ <?php
+ session_start();
+  $con = mysqli_connect("localhost", "root","","adoptors");
+
+  if (isset($_POST['log'])) {
+    $login_user = $_POST['email'];
+    $login_pword = $_POST['pword'];
+    $con_user = mysqli_query($con,"SELECT * FROM registration WHERE email = '$login_user' AND password = '$login_pword'");
+    $count = mysqli_num_rows($con_user);
+
+  
+  if ($count != 0) 
+  {
+    
+    
+   
+        echo '<script>';
+        echo 'alert("Logged In Successfully!");';
+        $_SESSION["loginuser"] = $login_user;
         
-          
+        echo 'window.location.href = "Main-Page.php";';
+        echo '</script>';
 
- <script>
-   $(document).ready(function() {
-    Materialize.updateTextFields();
-  });
-</script>
-		<!--Import jQuery before materialize.js-->
+  }
+  else
+  {
+      echo "<script> alert('Invalid Email!'); </script>";
+      // $result = mysqli_query($conn, "SELECT * FROM db_acct ORDER BY name");
+  }
+}
+?>
 
-   <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-      <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
-       <script type="text/javascript" src="materialize/js/materialize.js"></script>
+ 
     </body>
   </html>
--- register.php
+++ register.php
@@ -8,12 +8,27 @@
 
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+     
      <title>Register</title>
     </head>
 
     <body>
+  <script>
+    $(document).ready(function(){
 
-		<nav>
+        Materialize.updateTextFields();
+    });
+  </script>
+    <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+       <script type="text/javascript" src="materialize/js/materialize.js"></script>
+
+
+
+
+           
+<form name="form1" action="" method="post">
+    <nav>
     <div class="nav-wrapper blue-grey darken-1">
       <a href="Main-Page.php" class="brand-logo">Logo</a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
@@ -39,27 +54,28 @@
                   <div class="row white-text">
                     <br>
     <div class="input-field col s6">
-      <input id="lname" type="text" class="validate">
+      <input name="lname" type="text" class="validate">
       <label class="active white-text" for="lname" style="font-size: 20px;">Last Name</label>
     </div>
     <div class="input-field col s6">
-      <input id="fname" type="text" class="validate">
+      <input name="fname" type="text" class="validate">
       <label class="active white-text" for="fname" style="font-size: 20px;">First Name</label>
     </div>
     <div class="input-field col s12">
-      <input id="email" type="text" class="validate">
+      <input name="email" type="text" class="validate">
       <label class="active white-text" for="email" style="font-size: 20px;">Email</label>
     </div>
     <div class="input-field col s6">
-      <input id="pword" type="text" class="validate">
+      <input name="pword" type="text" class="validate">
       <label class="active white-text" for="pword" style="font-size: 20px;">Password</label>
     </div>
     <div class="input-field col s6">
-      <input id="conpword" type="text" class="validate">
+      <input name="conpword" type="text" class="validate">
       <label class="active white-text" for="conpword" style="font-size: 20px;">Confirm Password</label>
     </div>
     <div class="input-field col s8 offset-s5">
-      <a class="waves-effect waves-light btn blue-grey lighten-5 blue-grey-text text-darken-1" id="register" style="font-weight: bolder;">Register</a>
+      <a class="waves-effect waves-light btn blue-grey lighten-5 blue-grey-text text-darken-1" id="puta" style="font-weight: bolder;">Register</a>
+       <input class="btn btn-default submit " type="submit" name="register" value="tae">
     </div>
   </div>
             </div>
@@ -69,19 +85,32 @@
     </div>
        
 
-          
-      
+          <?php 
+    
+    if(isset($_POST["register"]))
+    {
+
+      $lname=$_POST['lname'];
+      $fname=$_POST['fname'];
+      $email=$_POST['email'];
+      $pword=$_POST['pword'];
+      $conpword=$_POST['conpword'];
+      $link=mysqli_connect("localhost","root","");
+      mysqli_select_db($link,"adoptors");
+      mysqli_query($link,"insert into registration values('$lname','$fname','$email','$pword','$conpword')");
+      echo mysqli_error($link);
+      ?>
+      <div class="alert alert-success col-lg-6 col-lg-push-3">
+        Registration successfully, You will get email when your account is approved
+    </div>
+  <?php
+    }
+  ?>
+    </div>
+x 
         
           
 
- <script>
-   $(document).ready(function() {
-    Materialize.updateTextFields();
-  });
-</script>
-		<!--Import jQuery before materialize.js-->
-     <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-      <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
-       <script type="text/javascript" src="materialize/js/materialize.js"></script>
+
     </body>
   </html>
