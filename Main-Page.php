7105f8ad6bc7f3673adc03acc6c3a68b822be18f
MICCAELA <MICCAELA>
2018-01-27 15:03:53 +0800

    script added mainpage

--- Main-Page.php
+++ Main-Page.php
@@ -11,11 +11,36 @@
     </head>
 
     <body>
-
-		
+<nav>
+    <div class="nav-wrapper blue-grey darken-1">
+      <a href="#" class="brand-logo">Logo</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="Sign-in.html">Sign in</a></li>
+        <li><a href="Register.html">Register   </a></li>
+        
+      </ul>
+    </div>
+  </nav>
+    
+    <br>
+    <div class="carousel">
+    <a class="carousel-item" href="#one!"><img src="https://lorempixel.com/250/250/nature/1"></a>
+    <a class="carousel-item" href="#two!"><img src="https://lorempixel.com/250/250/nature/2"></a>
+    <a class="carousel-item" href="#three!"><img src="https://lorempixel.com/250/250/nature/3"></a>
+    <a class="carousel-item" href="#four!"><img src="https://lorempixel.com/250/250/nature/4"></a>
+    <a class="carousel-item" href="#five!"><img src="https://lorempixel.com/250/250/nature/5"></a>
+  </div>
+      
+      <br>
           
-
-		<!--Import jQuery before materialize.js-->
+<script>
+  
+  $(document).ready(function(){
+      $('.carousel').carousel();
+    });
+        
+</script>
+    <!--Import jQuery before materialize.js-->
       <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
       <script type="text/javascript" src="js/materialize.min.js"></script>
     </body>
